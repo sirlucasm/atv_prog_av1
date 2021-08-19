d6fa6c77fc419e5bb361f14d61c51d9cc0bfb49b
Lucas Matheus <lucasmatheus2021@gmail.com>
2021-08-19 17:21:17 -0300

    added endpoints and new repositoryadded endpoints and new repository
    Signed-off-by: Lucas Matheus <lucasmatheus2021@gmail.com>

---
+++ src/main/java/com/atividades/atv_prog_av1/repository/PersonRepository.java
@@ -0,0 +1,4 @@
+package com.atividades.atv_prog_av1.repository;
+
+public interface PersonRepository extends org.springframework.data.jpa.repository.JpaRepository<com.atividades.atv_prog_av1.model.Person, java.lang.Long> {
+}
